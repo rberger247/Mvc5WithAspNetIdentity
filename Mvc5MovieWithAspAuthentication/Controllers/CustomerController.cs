59900f9b31e4a30f6a81dfb5a5287df558c55584
Rafael Berger <Rafiberger613@gmail.com>
2018-01-15 23:40:11 -0500

    Initial

---
+++ Mvc5MovieWithAspAuthentication/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc5MovieWithAspAuthentication.Models;
+
+namespace Mvc5MovieWithAspAuthentication.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        private  readonly MovieStoreEntities  db = new MovieStoreEntities();
+        // GET: Customer
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult NewCustomer()
+        {
+
+            var listOfCustomers = db.Customers.ToList();
+
+            return View(listOfCustomers);
+          
+        }
+    }
+}
---
+++ Mvc5MovieWithAspAuthentication/Models/MovieEntities.edmx.sql
@@ -0,0 +1,289 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 01/15/2018 23:20:34
+-- Generated from EDMX file: C:\src\Mvc5MovieWithAspAuthenticationSln\Mvc5MovieWithAspAuthentication\Models\MovieEntities.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [MovieStore];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_dbo_AspNetUserClaims_dbo_AspNetUsers_UserId]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[AspNetUserClaims] DROP CONSTRAINT [FK_dbo_AspNetUserClaims_dbo_AspNetUsers_UserId];
+GO
+IF OBJECT_ID(N'[dbo].[FK_dbo_AspNetUserLogins_dbo_AspNetUsers_UserId]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[AspNetUserLogins] DROP CONSTRAINT [FK_dbo_AspNetUserLogins_dbo_AspNetUsers_UserId];
+GO
+IF OBJECT_ID(N'[dbo].[FK_dbo_AspNetUserRoles_dbo_AspNetRoles_RoleId]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[AspNetUserRoles] DROP CONSTRAINT [FK_dbo_AspNetUserRoles_dbo_AspNetRoles_RoleId];
+GO
+IF OBJECT_ID(N'[dbo].[FK_dbo_AspNetUserRoles_dbo_AspNetUsers_UserId]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[AspNetUserRoles] DROP CONSTRAINT [FK_dbo_AspNetUserRoles_dbo_AspNetUsers_UserId];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[__MigrationHistory]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[__MigrationHistory];
+GO
+IF OBJECT_ID(N'[dbo].[AspNetRoles]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[AspNetRoles];
+GO
+IF OBJECT_ID(N'[dbo].[AspNetUserClaims]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[AspNetUserClaims];
+GO
+IF OBJECT_ID(N'[dbo].[AspNetUserLogins]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[AspNetUserLogins];
+GO
+IF OBJECT_ID(N'[dbo].[AspNetUserRoles]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[AspNetUserRoles];
+GO
+IF OBJECT_ID(N'[dbo].[AspNetUsers]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[AspNetUsers];
+GO
+IF OBJECT_ID(N'[dbo].[Customers]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Customers];
+GO
+IF OBJECT_ID(N'[dbo].[Movies]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Movies];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'AspNetRoles'
+CREATE TABLE [dbo].[AspNetRoles] (
+    [Id] nvarchar(128)  NOT NULL,
+    [Name] nvarchar(256)  NOT NULL
+);
+GO
+
+-- Creating table 'AspNetUserLogins'
+CREATE TABLE [dbo].[AspNetUserLogins] (
+    [LoginProvider] nvarchar(128)  NOT NULL,
+    [ProviderKey] nvarchar(128)  NOT NULL,
+    [UserId] nvarchar(128)  NOT NULL
+);
+GO
+
+-- Creating table 'AspNetUserRoles'
+CREATE TABLE [dbo].[AspNetUserRoles] (
+    [UserId] nvarchar(128)  NOT NULL,
+    [RoleId] nvarchar(128)  NOT NULL
+);
+GO
+
+-- Creating table 'AspNetUsers'
+CREATE TABLE [dbo].[AspNetUsers] (
+    [Id] nvarchar(128)  NOT NULL,
+    [Email] nvarchar(256)  NULL,
+    [EmailConfirmed] bit  NOT NULL,
+    [PasswordHash] nvarchar(max)  NULL,
+    [SecurityStamp] nvarchar(max)  NULL,
+    [PhoneNumber] nvarchar(max)  NULL,
+    [PhoneNumberConfirmed] bit  NOT NULL,
+    [TwoFactorEnabled] bit  NOT NULL,
+    [LockoutEndDateUtc] datetime  NULL,
+    [LockoutEnabled] bit  NOT NULL,
+    [AccessFailedCount] int  NOT NULL,
+    [UserName] nvarchar(256)  NOT NULL
+);
+GO
+
+-- Creating table 'C__MigrationHistory'
+CREATE TABLE [dbo].[C__MigrationHistory] (
+    [MigrationId] nvarchar(150)  NOT NULL,
+    [ContextKey] nvarchar(300)  NOT NULL,
+    [Model] varbinary(max)  NOT NULL,
+    [ProductVersion] nvarchar(32)  NOT NULL
+);
+GO
+
+-- Creating table 'AspNetUserClaims'
+CREATE TABLE [dbo].[AspNetUserClaims] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [UserId] nvarchar(128)  NOT NULL,
+    [ClaimType] nvarchar(max)  NULL,
+    [ClaimValue] nvarchar(max)  NULL
+);
+GO
+
+-- Creating table 'Customers'
+CREATE TABLE [dbo].[Customers] (
+    [Id] int  NOT NULL,
+    [Name] nvarchar(255)  NULL,
+    [MembershipTypeId] tinyint  NULL,
+    [BirthDate] datetime  NULL,
+    [IsSubscribedToNewsletter] bit  NULL
+);
+GO
+
+-- Creating table 'C__MigrationHistory1'
+CREATE TABLE [dbo].[C__MigrationHistory1] (
+    [MigrationId] nvarchar(150)  NOT NULL,
+    [ContextKey] nvarchar(300)  NOT NULL,
+    [Model] varbinary(max)  NOT NULL,
+    [ProductVersion] nvarchar(32)  NOT NULL
+);
+GO
+
+-- Creating table 'C__MigrationHistory2'
+CREATE TABLE [dbo].[C__MigrationHistory2] (
+    [MigrationId] nvarchar(150)  NOT NULL,
+    [ContextKey] nvarchar(300)  NOT NULL,
+    [Model] varbinary(max)  NOT NULL,
+    [ProductVersion] nvarchar(32)  NOT NULL
+);
+GO
+
+-- Creating table 'C__MigrationHistory3'
+CREATE TABLE [dbo].[C__MigrationHistory3] (
+    [MigrationId] nvarchar(150)  NOT NULL,
+    [ContextKey] nvarchar(300)  NOT NULL,
+    [Model] varbinary(max)  NOT NULL,
+    [ProductVersion] nvarchar(32)  NOT NULL
+);
+GO
+
+-- Creating table 'Movies'
+CREATE TABLE [dbo].[Movies] (
+    [Id] int  NOT NULL,
+    [Name] nvarchar(25)  NULL,
+    [GenreId] tinyint  NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'AspNetRoles'
+ALTER TABLE [dbo].[AspNetRoles]
+ADD CONSTRAINT [PK_AspNetRoles]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [LoginProvider], [ProviderKey], [UserId] in table 'AspNetUserLogins'
+ALTER TABLE [dbo].[AspNetUserLogins]
+ADD CONSTRAINT [PK_AspNetUserLogins]
+    PRIMARY KEY CLUSTERED ([LoginProvider], [ProviderKey], [UserId] ASC);
+GO
+
+-- Creating primary key on [UserId], [RoleId] in table 'AspNetUserRoles'
+ALTER TABLE [dbo].[AspNetUserRoles]
+ADD CONSTRAINT [PK_AspNetUserRoles]
+    PRIMARY KEY CLUSTERED ([UserId], [RoleId] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'AspNetUsers'
+ALTER TABLE [dbo].[AspNetUsers]
+ADD CONSTRAINT [PK_AspNetUsers]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [MigrationId], [ContextKey] in table 'C__MigrationHistory'
+ALTER TABLE [dbo].[C__MigrationHistory]
+ADD CONSTRAINT [PK_C__MigrationHistory]
+    PRIMARY KEY CLUSTERED ([MigrationId], [ContextKey] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'AspNetUserClaims'
+ALTER TABLE [dbo].[AspNetUserClaims]
+ADD CONSTRAINT [PK_AspNetUserClaims]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Customers'
+ALTER TABLE [dbo].[Customers]
+ADD CONSTRAINT [PK_Customers]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [MigrationId], [ContextKey] in table 'C__MigrationHistory1'
+ALTER TABLE [dbo].[C__MigrationHistory1]
+ADD CONSTRAINT [PK_C__MigrationHistory1]
+    PRIMARY KEY CLUSTERED ([MigrationId], [ContextKey] ASC);
+GO
+
+-- Creating primary key on [MigrationId], [ContextKey] in table 'C__MigrationHistory2'
+ALTER TABLE [dbo].[C__MigrationHistory2]
+ADD CONSTRAINT [PK_C__MigrationHistory2]
+    PRIMARY KEY CLUSTERED ([MigrationId], [ContextKey] ASC);
+GO
+
+-- Creating primary key on [MigrationId], [ContextKey] in table 'C__MigrationHistory3'
+ALTER TABLE [dbo].[C__MigrationHistory3]
+ADD CONSTRAINT [PK_C__MigrationHistory3]
+    PRIMARY KEY CLUSTERED ([MigrationId], [ContextKey] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Movies'
+ALTER TABLE [dbo].[Movies]
+ADD CONSTRAINT [PK_Movies]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [RoleId] in table 'AspNetUserRoles'
+ALTER TABLE [dbo].[AspNetUserRoles]
+ADD CONSTRAINT [FK_dbo_AspNetUserRoles_dbo_AspNetRoles_RoleId]
+    FOREIGN KEY ([RoleId])
+    REFERENCES [dbo].[AspNetRoles]
+        ([Id])
+    ON DELETE CASCADE ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_dbo_AspNetUserRoles_dbo_AspNetRoles_RoleId'
+CREATE INDEX [IX_FK_dbo_AspNetUserRoles_dbo_AspNetRoles_RoleId]
+ON [dbo].[AspNetUserRoles]
+    ([RoleId]);
+GO
+
+-- Creating foreign key on [UserId] in table 'AspNetUserClaims'
+ALTER TABLE [dbo].[AspNetUserClaims]
+ADD CONSTRAINT [FK_dbo_AspNetUserClaims_dbo_AspNetUsers_UserId]
+    FOREIGN KEY ([UserId])
+    REFERENCES [dbo].[AspNetUsers]
+        ([Id])
+    ON DELETE CASCADE ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_dbo_AspNetUserClaims_dbo_AspNetUsers_UserId'
+CREATE INDEX [IX_FK_dbo_AspNetUserClaims_dbo_AspNetUsers_UserId]
+ON [dbo].[AspNetUserClaims]
+    ([UserId]);
+GO
+
+-- Creating foreign key on [UserId] in table 'AspNetUserLogins'
+ALTER TABLE [dbo].[AspNetUserLogins]
+ADD CONSTRAINT [FK_dbo_AspNetUserLogins_dbo_AspNetUsers_UserId]
+    FOREIGN KEY ([UserId])
+    REFERENCES [dbo].[AspNetUsers]
+        ([Id])
+    ON DELETE CASCADE ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_dbo_AspNetUserLogins_dbo_AspNetUsers_UserId'
+CREATE INDEX [IX_FK_dbo_AspNetUserLogins_dbo_AspNetUsers_UserId]
+ON [dbo].[AspNetUserLogins]
+    ([UserId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
